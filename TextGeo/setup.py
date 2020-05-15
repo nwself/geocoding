e2a40f09df2e8805b578d41281e3c83852dfe086
Nathan Self <nwself@cs.vt.edu>
2020-05-14 22:11:08 -0400

    Refactor to package textgeo module

---
+++ TextGeo/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="textgeo", # Replace with your own username
+    version="0.0.1",
+    author="Sathappan Muthiah",
+    author_email="sathap1@vt.edu",
+    description="Geocode text content",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sathappanspm/geocoding",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'elasticsearch-dsl>=7.0.0,<8.0.0',
+        'gevent',
+        'nltk',
+        'numpy',
+        'unicodecsv',
+        'Unidecode',
+    ],
+    package_data={
+        'textgeo': [
+            'geoutils/Admin2Info.json',
+            'geoutils/adminInfo.json',
+            'geoutils/countryInfo.json',
+            'geoutils/stopwords.txt',
+        ]
+    }
+)
---
+++ TextGeo/textgeo/setup.sh
@@ -0,0 +1,19 @@
+CUR_DIR=`pwd`
+
+if [ -d "gnames_gaz" ]
+then
+    rm -rf gnames_gaz
+fi
+
+mkdir gnames_gaz ;
+cd gnames_gaz;
+
+wget http://download.geonames.org/export/dump/admin2Codes.txt
+wget http://download.geonames.org/export/dump/admin1CodesASCII.txt
+wget http://download.geonames.org/export/dump/countryInfo.txt
+wget http://download.geonames.org/export/dump/allCountries.zip
+unzip allCountries.zip
+cd $CUR_DIR
+curl -XPUT 'localhost:9200/geonames' -H 'Content-Type: application/json' -d @./data/es_settings.json
+python initialize.py
+#rm -rf gnames_gaz
